fea9d2e71426cb4893a74ff432afebc32749b3c7
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2022-12-14 17:38:14 +0100

    got element from DOM + array list

---
+++ script.js
@@ -0,0 +1,11 @@
+/* Exercise
+Data una lista della spesa (inventatela), stampare in pagina gli elementi della lista individualmente con un ciclo while. 
+(anche brutalmente, basta che si vedano)
+*/
+
+// DOM's elements
+const resultElement = document.getElementById('result');
+
+// shopping list
+const shoppingList = ['pane', 'latte', 'uova', 'carote', 'carne'];
+
